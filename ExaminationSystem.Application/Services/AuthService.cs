78a798eab00d0ae29fca31d2274c678f7780b62a
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-04-14 10:04:03 +0200

    Add Register function in service without Refresh token part

--- ExaminationSystem.Application/Services/AuthService.cs
+++ ExaminationSystem.Application/Services/AuthService.cs
@@ -1,10 +1,12 @@
-﻿using ExaminationSystem.Domain.Dtos.Account;
+﻿using ExaminationSystem.Application.Helpers;
+using ExaminationSystem.Domain.Dtos.Account;
 using ExaminationSystem.Domain.Entities;
 using ExaminationSystem.Domain.Services.contract;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,9 +31,74 @@ namespace ExaminationSystem.Application.Services
             throw new NotImplementedException();
         }
 
-        public Task<AuthResponseDto> Register(RegisterDto registerDto)
+        public async Task<AuthResponseDto> Register(RegisterDto registerDto)
         {
-            throw new NotImplementedException();
+            if ( await _userManager.FindByEmailAsync(registerDto.Email) is not null)
+            {
+                return new AuthResponseDto()
+                {
+                    Message = "Email is already registered!"
+                };
+            }
+
+            if( await _userManager.FindByNameAsync(registerDto.UserName) is not null)
+            {
+                return new AuthResponseDto()
+                {
+                    Message = "Username is already registered!"
+                };
+            }
+
+            var user = registerDto.Mapone<AppUser>();
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if(!result.Succeeded)
+            {
+                var errors = string.Empty;
+
+                foreach (var error in result.Errors)
+                {
+                    errors += $"{error.Description} ,";
+                }
+
+                return new AuthResponseDto()
+                {
+                    Message = errors
+                };
+            }
+
+            if(registerDto.Type == "Instructor")
+            {
+                await _userManager.AddToRoleAsync(user, "Instructor");
+            }
+            else if(registerDto.Type == "Student")
+            {
+                await _userManager.AddToRoleAsync(user, "Student");
+            }
+
+            var token = CreateJWTToken(user);
+
+            #region Refresh Token Part
+
+            #endregion
+
+            return new AuthResponseDto()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                ExpiresOn = token.ValidTo,
+                IsAuthenticated = true,
+                Message = "Registration Succedded",
+                Roles = new List<string>() { "User"},
+
+            };
+
+        }
+
+
+        public JwtSecurityToken CreateJWTToken(AppUser user)
+        {
+
         }
     }
 }
