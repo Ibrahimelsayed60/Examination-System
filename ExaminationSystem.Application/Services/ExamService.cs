755e41b2f12737e744543b765fcc2fcd117271f5
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-04-18 16:33:37 +0200

    Add Exam Service implementation

---
+++ ExaminationSystem.Application/Services/ExamService.cs
@@ -0,0 +1,76 @@
+﻿using ExaminationSystem.Application.Helpers;
+using ExaminationSystem.Domain.Dtos.Exam;
+using ExaminationSystem.Domain.Entities;
+using ExaminationSystem.Domain.Repositories.contract;
+using ExaminationSystem.Domain.Services.contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExaminationSystem.Application.Services
+{
+    public class ExamService : IExamService
+    {
+        private readonly IBaseRepository<Exam> _examRepo;
+
+        public ExamService(IBaseRepository<Exam> examRepo) 
+        {
+            _examRepo = examRepo;
+        }
+
+        public async Task<IEnumerable<ExamDto>> GetAllExams()
+        {
+            return (await _examRepo.GetAllAsync()).Map<ExamDto>().ToList();
+        }
+
+        public async Task<ExamDto> GetExamById(int id)
+        {
+            return (await _examRepo.GetByIdAsync(id)).Mapone<ExamDto>();
+        }
+
+        public async Task<int> CreateExam(ExamCreateDto examCreateDto)
+        {
+            var exam = await _examRepo.AddAsync(examCreateDto.Mapone<Exam>());
+
+            await _examRepo.SaveChangesAsync();
+
+            return exam.Id;
+        }
+
+
+        public async Task UpdateExam(ExamUpdateDto examUpdateDto)
+        {
+            var exam = await _examRepo.GetByIdAsync(examUpdateDto.Id);
+
+            if (exam is null)
+            {
+                throw new Exception("Exam Not Found");
+            }
+
+            exam = examUpdateDto.Mapone<Exam>();
+
+            _examRepo.update(exam);
+
+            await _examRepo.SaveChangesAsync();
+        }
+
+        public async Task DeleteExam(int id)
+        {
+            var exam = await _examRepo.getWithTrackingByIdAsync(id);
+
+            if(exam is null)
+            {
+                throw new Exception("Exam is Not found");
+            }
+
+            _examRepo.Delete(exam);
+
+            await _examRepo.SaveChangesAsync();
+
+        }
+
+        
+    }
+}
---
+++ ExaminationSystem.Core/Services.contract/IExamService.cs
@@ -0,0 +1,21 @@
+﻿using ExaminationSystem.Domain.Dtos.Exam;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExaminationSystem.Domain.Services.contract
+{
+    public interface IExamService
+    {
+        Task<IEnumerable<ExamDto>> GetAllExams();
+
+        Task<ExamDto> GetExamById(int id);
+
+        Task<int> CreateExam(ExamCreateDto examCreateDto);
+        Task UpdateExam(ExamUpdateDto examUpdateDto);
+        Task DeleteExam(int id);
+
+    }
+}
